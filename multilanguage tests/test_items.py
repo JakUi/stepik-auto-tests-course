21755ea287bfd1cf66ff4901d6ed0635c5c237fd
JakUi <48787844+JakUi@users.noreply.github.com>
2020-03-02 11:14:22 +0300

    Тест проверяет наличие кнопки "Добавить в корзину"
    Выполняется на языке, который указал пользователь иначе - выполняется на английском.

--- multilanguage tests/test_items.py
+++ multilanguage tests/test_items.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
+import time
 
 def test_add_to_cart_button(browser):
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
-    add_2_cart = browser.find_element_by_class_name("btn.btn-lg.btn-primary.btn-add-to-basket")
-    assert add_2_cart != None, 'Add to Cart button not found!'
+    time.sleep(30.0)
+    add_2_cart = browser.find_element_by_class_name("btn.btn-lg.btn-primary.btn-add-to-basket") #Find "Add to cart" button
+    assert add_2_cart != None, 'Add to Cart button not found'
