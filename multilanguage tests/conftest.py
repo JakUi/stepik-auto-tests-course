6647c37dbdf079128ad679fcbc124d1f2ab2bd74
JakUi <48787844+JakUi@users.noreply.github.com>
2020-03-02 10:56:13 +0300

    Conftest файл
    Для запуска используйте команду pytest --language=es test_items.py
    Если параметр '--language' будет отличным от 'es' - сайт запустится на английском языке

---
+++ multilanguage tests/conftest.py
@@ -0,0 +1,23 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose browser language")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
+    browser.implicitly_wait(25.0)
+    if user_language == 'es':
+        print("\n Run test on Espaniol")
+    elif user_language != "es":
+        print("\n Run test on English")
+        user_language = 'en'
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
